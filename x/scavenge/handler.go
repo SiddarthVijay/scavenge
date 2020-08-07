f356c07d0ee24e035684f4cd92bbc4c66e712eed
Siddarth Vijay <vsiddarthvijay@gmail.com>
2020-08-07 17:14:20 +0530

    working on handler

--- x/scavenge/handler.go
+++ x/scavenge/handler.go
@@ -12,16 +12,55 @@ func NewHandler(k Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 		switch msg := msg.(type) {
-		// TODO: Define your msg cases
-		// 
-		//Example:
-		// case Msg<Action>:
-		// 	return handleMsg<Action>(ctx, k, msg)
+		case MsgCreateScavenge:
+			return handleMsgCreateScavenge(ctx, k, msg)
+		case MsgCommitSolution:
+			return handleMsgCommitSolution(ctx, k, msg)
+		case MsgRevealSolution:
+			return handleMsgRevealSolution(ctx, k, msg)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", ModuleName,  msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
 		}
 	}
+}
+
+// handle<Action> does x
+func handleMsg<Action>(ctx sdk.Context, k Keeper, msg Msg<Action>) (*sdk.Result, error) {
+	err := k.<Action>(ctx, msg.ValidatorAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Define your msg events
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.ValidatorAddr.String()),
+		),
+	)
+
+	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
+}
+
+// handle<Action> does x
+func handleMsg<Action>(ctx sdk.Context, k Keeper, msg Msg<Action>) (*sdk.Result, error) {
+	err := k.<Action>(ctx, msg.ValidatorAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Define your msg events
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.ValidatorAddr.String()),
+		),
+	)
+
+	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
 }
 
 // handle<Action> does x
